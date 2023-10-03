0fe6e24c56be98c53a87dba69026b2f40b03292c
Revenant <dasdev.pratik@gmail.com>
2023-10-03 16:44:44 +0530

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { Toaster } from "react-hot-toast";
+
 function App() {
+  const [tickets, setTickets] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [input, setInput] = useState('');
+  const [error, setError] = useState('');
 
-  const [tickets, setTickets] = useState([])
   useEffect(() => {
-    async function fetchData(){
-      const res = await fetch("https://kiitmunbackend.onrender.com/api/tickets")
-      const data = await res.json()
-      setTickets(data)
+    async function fetchData() {
+      setLoad(true);
+      const res = await fetch(
+        "https://kiitmunbackend.onrender.com/api/tickets"
+      );
+      const data = await res.json();
+      setLoad(false);
+      setTickets(data);
     }
-    fetchData()
-  }, [tickets])
+    return () => fetchData();
+  }, []);
 
-//  
-// fetch('https://jsonplaceholder.typicode.com/posts/1', requestOptions)
-//     .then(response => response.json())
-//     .then(data => this.setState({ postId: data.id }));
-// }
-  
-
-const updateResolve = (id)=>{
-  const requestOptions = {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
+  const updateResolve = async (id, name, subject) => {
+    const secret = window.prompt("Enter secret key", "");
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: "resolved", name, subject, authCode: secret }),
     };
-    fetch('https://kiitmunbackend.onrender.com/api/tickets/:id', requestOptions)
-        .then(response => response.json())
-        .then(data => this.setState({ postId: data.id }));
-}
+    setLoad(true);
+    const updateFetch = await fetch(
+      `https://kiitmunbackend.onrender.com/api/tickets/${id}`,
+      requestOptions
+    );
+
+    const data = await updateFetch.json();
+    const newTickets = tickets.map((ticket) =>
+      ticket.id === id ? { ...ticket, status: data.status } : ticket
+    );
+    setLoad(false);
+    setTickets(newTickets);
+    return console.log(data);
+  };
+
+  const updateUnResolved = async(id,data, name, subject)=>{
+    const secret = window.prompt("Enter secret key", "");
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: "unresolved", name, subject, authCode: secret, replyMessage: input}),
+    };
+    console.log(secret);
+    setLoad(true);
+    const updateFetch = await fetch(
+      `https://kiitmunbackend.onrender.com/api/tickets/${id}`,
+      requestOptions
+    );
+
+    const putdata = await updateFetch.json();
+    const newTickets = tickets.map((ticket) =>
+      ticket.id === id ? { ...ticket, status: putdata.status } : ticket
+    );
+    setLoad(false);
+    setTickets(newTickets);
+    return console.log(data);
+  }
+
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <Toaster position="bottom-center" reverseOrder={true} />
       <h1 className="text-center my-8">Issue form MUN</h1>
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -47,47 +86,75 @@ const updateResolve = (id)=>{
               Status
             </th>
             <th scope="col" className="px-6 py-3">
-              Resolved
+              Resolve button
             </th>
             <th scope="col" className="px-6 py-3">
-              Response to be sent if unresolved
+              Response, if unresolved
             </th>
           </tr>
         </thead>
         <tbody>
-          {tickets?.map((ticket)=>
-               <tr key={ticket.id} className="bg-gray-900 border-gray-700">
-               <th
-                 scope="row"
-                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-               >
-                 {ticket.id}
-               </th>
-               <td className="px-6 py-4">{ticket.name}</td>
-               <td className="px-6 py-4">{ticket.subject}</td>
-               <td className="px-6 py-4">{ticket.status ? <>{ticket.status}</>: <>pending</>}</td>
-               <td className="px-6 py-4">
-                 <button
-                   className="font-medium bg-blue-400 text-white rounded-md hover:underline"
-                 >
-                   Send email and update
-                 </button>
-               </td>
-               <td className="px-6 py-4">
-                 <input
-                   type="text"
-                   id="message"
-                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   placeholder="John"
-                   required
-                 />{" "}
-                 <button onClick={updateResolve} className="bg-red-500 p-2 rounded-md text-white">
-                   Send and update
-                 </button>
-               </td>
-             </tr>
-          )}
-       
+          {tickets?.map((ticket) => (
+            <tr key={ticket.id} className="bg-gray-900 border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {ticket.id}
+              </th>
+              <td className="px-6 py-4">{ticket.name}</td>
+              <td className="px-6 py-4">{ticket.subject}</td>
+              <td className="px-6 py-4">
+                {ticket.status ? <>{ticket.status}</> : <>pending</>}
+              </td>
+              <td className="px-6 py-4">
+                {load ? (
+                  <></>
+                ) : ticket.status === "resolved" ? (
+                  <></>
+                ) : ticket.status === "unresolved" ? (
+                  <></>
+                ) : (
+                  <button
+                    className="font-medium bg-blue-400 text-white rounded-md hover:underline"
+                    onClick={() => updateResolve(ticket.id, ticket.name, ticket.subject)}
+                  >
+                    Send email and update
+                  </button>
+                )}
+              </td>
+              <td className="px-6 py-4">
+                {load ? (
+                  <></>
+                ) : (
+                  <div>
+                    {ticket.status === "resolved" ? (
+                      <></>
+                    ) : ticket.status === "unresolved" ? (
+                      <></>
+                    ) : (
+                      <form onSubmit={(e)=>{
+                        e.preventDefault()
+                        updateUnResolved(ticket.id, input, ticket.name, ticket.subject)
+                        }}>
+                        <input
+                          type="text"
+                          id="message"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          placeholder="John"
+                          required
+                          onChange={(e)=>setInput(e.target.value)}
+                        />
+                        <button type="submit" className="bg-red-500 p-2 rounded-md text-white">
+                          Send and update
+                        </button>
+                      </form>
+                    )}
+                  </div>
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
